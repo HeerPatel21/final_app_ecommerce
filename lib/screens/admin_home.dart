3a2d8efa1ee676ccfce54116d52bf0616f8f2a4d
FlutLab.io <>
2022-01-19 06:23:21 +0000

    Autosave

--- lib/screens/admin_home.dart
+++ lib/screens/admin_home.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class AdminHomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Center(child: Text('Admin')),
+    );
   }
 }
