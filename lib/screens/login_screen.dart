6131ee6266c3c3a17055efcc80e426e38e7040c6
FlutLab.io <>
2022-01-19 06:39:57 +0000

    Autosave

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -5,6 +5,18 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromRGBO(255,138,120,1),
+              Color.fromRGBO(255,114,117,1),
+              Color.fromRGBO(355,63,111,1),
+
+            ]
+          )
+        )
         child: Center(child: Text('Login')),
       ), //container
     ); //scaffold
