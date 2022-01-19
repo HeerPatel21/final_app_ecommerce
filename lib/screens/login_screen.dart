9dea217c2de359c894edc5f0b7a4e5ff8c3a44cb
FlutLab.io <>
2022-01-19 06:35:46 +0000

    Autosave

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -3,8 +3,10 @@ import 'package:flutter/material.dart';
 class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Scaffold(
+      body: Container(
       child: Center(child: Text('Login')),
+    );
     );
   }
 }
