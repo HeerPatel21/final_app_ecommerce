10cb3b2c1b1c30be7d04ef4ddef7d928648f4350
FlutLab.io <>
2022-01-19 06:48:47 +0000

    Autosave

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,27 +1,25 @@
 import 'package:flutter/material.dart';
 
-
 class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromRGBO(255, 138, 120, 1),
-                Color.fromRGBO(255, 114, 117, 1),
-                Color.fromRGBO(355, 63, 111, 1),
-              ],
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-            ), //lineargradient
-          ), //BoxDecoration
-          child: Column(children: [
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromRGBO(255, 138, 120, 1),
+              Color.fromRGBO(255, 114, 117, 1),
+              Color.fromRGBO(355, 63, 111, 1),
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ), //lineargradient
+        ), //BoxDecoration
+        child: Column(
+          children: [
             Text(
               'Hemaxi',
               style: TextStyle(
@@ -29,8 +27,10 @@ class LoginScreen extends StatelessWidget {
                 fontWeight: FontWeight.bold,
                 color: Colors.white,
               ), //textStyle
-            ), //text            
-          ],),), //container
+            ), //text
+          ],
+        ),
+      ), //container
     ); //scaffold
   }
 }
