2d5195eb3a571b4742f3da806be7f0628553092a
FlutLab.io <>
2022-01-23 13:16:50 +0000

    Autosave

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -7,19 +7,18 @@ class SignUpScreen extends StatelessWidget {
       body: Container(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
-        
         decoration: BoxDecoration(
           gradient: LinearGradient(
             colors: [
-              Color.fromRGBO(255,138,120,1),
-              Color.fromRGBO(255,114,117,1),
-              Color.fromRGBO(355,63,111,1),
+              Color.fromRGBO(255, 138, 120, 1),
+              Color.fromRGBO(255, 114, 117, 1),
+              Color.fromRGBO(355, 63, 111, 1),
             ],
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
           ),
         ),
-      );
+      ),
     );
-  } 
+  }
 }
