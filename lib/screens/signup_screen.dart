8a1b329bdc2ef5f2edc38b40d25afc3124103995
FlutLab.io <>
2022-01-23 13:09:22 +0000

    Autosave

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -3,8 +3,20 @@ import 'package:flutter/material.dart';
 class SignUpScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(child: Text('Sign Up')),
+    return Scaffold(
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromRGBO(255,138,120,1),
+              Color.fromRGBO(255,114,117,1),
+              Color.fromRGBO(355,63,111,1),
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+      );
     );
-  }
+  } 
 }
