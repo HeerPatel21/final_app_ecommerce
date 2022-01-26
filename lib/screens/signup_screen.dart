773b8613b2e9629914ef8f9aec90f5e77839c00a
FlutLab.io <>
2022-01-26 05:54:53 +0000

    Autosave

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -61,113 +61,115 @@ class _SignUpScreenState extends State<SignUpScreen> {
   }
 
   Widget _buildSignUpForm() {
-    return Column(
-      children: [
-        SizedBox(
-          height: 60,
-        ),
-        Container(
-          margin: EdgeInsets.symmetric(horizontal: 40),
-          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(40),
-          ), //BoxDecoration
-          child: TextFormField(
-            validator: (value) {
-              return null;
-            },
-            onSaved: (newValue) {
-              _users.displayName = newValue;
-            },
-            keyboardType: TextInputType.name,
-            cursorColor: Color.fromRGBO(255, 63, 111, 1),
-            decoration: InputDecoration(
-              hintText: 'User Name',
-              hintStyle: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Color.fromRGBO(255, 63, 111, 1),
-              ), //textStyle
-              icon: Icon(
-                Icons.account_circle,
-                color: Color.fromRGBO(255, 63, 111, 1),
-              ),
-              enabledBorder: InputBorder.none,
-              focusedBorder: InputBorder.none,
-            ), //inputDecoration
-          ), //textFormField
-        ), //container
-        SizedBox(
-          height: 20,
-        ),
-        //email Field
-        Container(
-          margin: EdgeInsets.symmetric(horizontal: 40),
-          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(40),
-          ), //BoxDecoration
-          child: TextFormField(
-            validator: (value) {
-              return null;
-            },
-            onSaved: (newValue) {
-              _users.email = newValue;
-            },
-            keyboardType: TextInputType.emailAddress,
-            cursorColor: Color.fromRGBO(255, 63, 111, 1),
-            decoration: InputDecoration(
-              hintText: 'Email',
-              hintStyle: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Color.fromRGBO(255, 63, 111, 1),
-              ), //textStyle
-              icon: Icon(
-                Icons.email,
-                color: Color.fromRGBO(255, 63, 111, 1),
-              ),
-              enabledBorder: InputBorder.none,
-              focusedBorder: InputBorder.none,
-            ), //inputDecoration
-          ), //textFormField
-        ), //container
-        SizedBox(
-          height: 20,
-        ),
-        //phone Field
-        Container(
-          margin: EdgeInsets.symmetric(horizontal: 40),
-          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(40),
-          ), //BoxDecoration
-          child: TextFormField(
-            validator: (value) {
-              return null;
-            },
-            onSaved: (newValue) {
-              _users.phone = newValue;
-            },
-            keyboardType: TextInputType.phone,
-            cursorColor: Color.fromRGBO(255, 63, 111, 1),
-            decoration: InputDecoration(
-              hintText: 'Phone Number',
-              hintStyle: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Color.fromRGBO(255, 63, 111, 1),
-              ), //textStyle
-              icon: Icon(
-                Icons.phone,
-                color: Color.fromRGBO(255, 63, 111, 1),
-              ),
-              enabledBorder: InputBorder.none,
-              focusedBorder: InputBorder.none,
-            ), //inputDecoration
-          ), //textFormField
-        ), //container
-      ],
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          SizedBox(
+            height: 60,
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 40),
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(40),
+            ), //BoxDecoration
+            child: TextFormField(
+              validator: (value) {
+                return null;
+              },
+              onSaved: (newValue) {
+                _users.displayName = newValue;
+              },
+              keyboardType: TextInputType.name,
+              cursorColor: Color.fromRGBO(255, 63, 111, 1),
+              decoration: InputDecoration(
+                hintText: 'User Name',
+                hintStyle: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromRGBO(255, 63, 111, 1),
+                ), //textStyle
+                icon: Icon(
+                  Icons.account_circle,
+                  color: Color.fromRGBO(255, 63, 111, 1),
+                ),
+                enabledBorder: InputBorder.none,
+                focusedBorder: InputBorder.none,
+              ), //inputDecoration
+            ), //textFormField
+          ), //container
+          SizedBox(
+            height: 20,
+          ),
+          //email Field
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 40),
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(40),
+            ), //BoxDecoration
+            child: TextFormField(
+              validator: (value) {
+                return null;
+              },
+              onSaved: (newValue) {
+                _users.email = newValue;
+              },
+              keyboardType: TextInputType.emailAddress,
+              cursorColor: Color.fromRGBO(255, 63, 111, 1),
+              decoration: InputDecoration(
+                hintText: 'Email',
+                hintStyle: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromRGBO(255, 63, 111, 1),
+                ), //textStyle
+                icon: Icon(
+                  Icons.email,
+                  color: Color.fromRGBO(255, 63, 111, 1),
+                ),
+                enabledBorder: InputBorder.none,
+                focusedBorder: InputBorder.none,
+              ), //inputDecoration
+            ), //textFormField
+          ), //container
+          SizedBox(
+            height: 20,
+          ),
+          //phone Field
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 40),
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(40),
+            ), //BoxDecoration
+            child: TextFormField(
+              validator: (value) {
+                return null;
+              },
+              onSaved: (newValue) {
+                _users.phone = newValue;
+              },
+              keyboardType: TextInputType.phone,
+              cursorColor: Color.fromRGBO(255, 63, 111, 1),
+              decoration: InputDecoration(
+                hintText: 'Phone Number',
+                hintStyle: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromRGBO(255, 63, 111, 1),
+                ), //textStyle
+                icon: Icon(
+                  Icons.phone,
+                  color: Color.fromRGBO(255, 63, 111, 1),
+                ),
+                enabledBorder: InputBorder.none,
+                focusedBorder: InputBorder.none,
+              ), //inputDecoration
+            ), //textFormField
+          ), //container
+        ],
+      ),
     );
   }
 }
