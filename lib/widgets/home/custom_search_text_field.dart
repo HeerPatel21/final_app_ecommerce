6c5f0af74f8939d7096057691444425691fc6dbf
FlutLab.io <>
2022-02-01 05:51:59 +0000

    Autosave

--- lib/widgets/home/custom_search_text_field.dart
+++ lib/widgets/home/custom_search_text_field.dart
@@ -10,6 +10,17 @@ class CustomSearchTextField extends StatelessWidget {
       decoration: BoxDecoration(
         color: Colors.purple.withOpacity(0.33),
         borderRadius: BorderRadius.circular(15),
+      ), //BoxDecoration
+      child: TextField(
+        textInputAction: TextInputAction.search,
+        decoration: InputDecoration(
+          enabledBorder: InputBorder.none,
+          focusedBorder: InputBorder.none,
+          hintText: "Search Product",
+          hintStyle: TextStyle(color: Color(0xFF800f2f), fontSize: 20),
+          prefixIcon: Icon(Icons.search),
+          contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+        ),
       ),
     );
   }
