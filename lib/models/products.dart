2421957bf357fdb4c3ce8e017f099e911b72902b
FlutLab.io <>
2022-02-01 06:37:12 +0000

    Autosave

---
+++ lib/models/products.dart
@@ -0,0 +1,8 @@
+enum ProductType {
+  Mayur_Materials,
+  Pranjul_Materials,
+  Diptex_Materials,
+  Work_Materials,
+  Bandhini_Materials,
+  All_Others,
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import '../widgets/home/header_section.dart';
+import '../models/products.dart';
 
 class HomeScreen extends StatelessWidget {
+  final productCategories = <Map>[
+    <String, dynamic>{
+      'icon': 'assets/icons/mayur_creation.jpg',
+      'title': 'Mayur Dress Materials',
+      'product_type': ProductType.Mayur_Materials,
+    }
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
