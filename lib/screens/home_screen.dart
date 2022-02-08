66bb763535eefc84a3b11648d48f47a98882b4b9
FlutLab.io <>
2022-02-08 06:40:10 +0000

    Autosave

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,32 +10,32 @@ const String PRODUCT_TYPE_KEY = 'product_type';
 class HomeScreen extends StatelessWidget {
   final productCategories = <Map>[
     <String, dynamic>{
-      ICON_KEY: 'assets/icons/mayur_creation.jpg',
+      ICON_KEY: 'assets/icons/Electronics.svg',
       TITLE_KEY: 'Mayur Dress Materials',
       PRODUCT_TYPE_KEY: ProductType.Mayur_Materials,
     },
     <String, dynamic>{
-      ICON_KEY: 'assets/icons/pranjul_materials.jpg',
+      ICON_KEY: 'assets/icons/Groceries.svg',
       TITLE_KEY: 'Pranjul Dress Materials',
       PRODUCT_TYPE_KEY: ProductType.Pranjul_Materials,
     },
     <String, dynamic>{
-      ICON_KEY: 'assets/icons/diptex_materials.jpg',
+      ICON_KEY: 'assets/icons/Art.svg',
       TITLE_KEY: 'Diptex Dress Materials',
       PRODUCT_TYPE_KEY: ProductType.Diptex_Materials,
     },
     <String, dynamic>{
-      ICON_KEY: 'assets/icons/work_materials.jpg',
+      ICON_KEY: 'assets/icons/Others.svg',
       TITLE_KEY: 'Work Dress Materials',
       PRODUCT_TYPE_KEY: ProductType.Work_Materials,
     },
     <String, dynamic>{
-      ICON_KEY: 'assets/icons/bandhini_materials.jpg',
+      ICON_KEY: 'assets/icons/Others.svg',
       TITLE_KEY: 'Bandhini Dress Materials',
       PRODUCT_TYPE_KEY: ProductType.Bandhini_Materials,
     },
     <String, dynamic>{
-      ICON_KEY: 'assets/icons/materials_materials.jpg',
+      ICON_KEY: 'assets/icons/Art.svg',
       TITLE_KEY: 'Other Company Materials',
       PRODUCT_TYPE_KEY: ProductType.Others,
     },
