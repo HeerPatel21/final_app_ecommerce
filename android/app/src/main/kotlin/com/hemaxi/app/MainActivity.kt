cc1a19b93bafcdab3bf3c24b738957b93131a956
FlutLab.io <>
2022-02-11 07:01:39 +0000

    Switching to branch day16

--- android/app/src/main/kotlin/com/hemaxi/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/hemaxi/app/MainActivity.kt
@@ -4,7 +4,21 @@ import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
+import com.google.android.gms.ads.MobileAds
 
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        MobileAds.initialize(this) {}
+    }
+}
+override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    // ...
+    MobileAds.initialize(this)
+}
 class MainActivity: FlutterActivity() {
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine);
