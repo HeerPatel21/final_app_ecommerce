a6d4d000e2e7560d9f2b0c45e82e2e23a04b7361
FlutLab.io <>
2022-02-14 06:08:29 +0000

    Autosave

--- lib/widgets/categories/category_product.dart
+++ lib/widgets/categories/category_product.dart
@@ -12,22 +12,26 @@ class _CategoryProductState extends State<CategoryProduct> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20),
-      child: Column(children: [
-        //header section
-        SizedBox(height: 40),
-        Row(children: [
-          IconButton(            
-            icon: Icon(
-              Icons.west_outlined,
-              color: Color(0xFF800f2f),
-            ),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          Expanded(child:CustomSearchTextField(onSubmitted: (value) {})),
-          ),
-        ],)
-      ],),
+      child: Column(
+        children: [
+          //header section
+          SizedBox(height: 40),
+          Row(
+            children: [
+              IconButton(
+                icon: Icon(
+                  Icons.west_outlined,
+                  color: Color(0xFF800f2f),
+                ),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+              Expanded(child: CustomSearchTextField(onSubmitted: (value) {})),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
